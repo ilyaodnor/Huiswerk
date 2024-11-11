b35a5c6baed0555e7dce75a24fe3039063159cb8
Ваше имя <99083003@mydavinci.nl>
2024-11-11 10:44:37 +0100

    6.

--- periode 3/week26/lijsten.py
+++ periode 3/week26/lijsten.py
@@ -2,3 +2,6 @@ lijst = [5,12,19,27,3]
 lijst.append(25)
 print(lijst)
 print(f'Er zitten {len(lijst)} elementen in de lijst.')
+lijst.remove(lijst[1])
+cijfer = lijst.pop(0)
+print(f'Het nummer {cijfer} is verwijderd')
